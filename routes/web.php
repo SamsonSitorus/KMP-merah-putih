b67a7c6ee17010855f586986bc75f196f0b5bcec
SamsonSitorus <149450441+SamsonSitorus@users.noreply.github.com>
2025-09-18 14:22:42 +0700

    t
    t

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Authcontroller;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+/*Route Login */
+Route::get('/login', fn() => view('auth.login'))->name('login');
+Route::post('/login', [Authcontroller::class,'login']);
+
+Route::get('/register', fn() => view('auth.auth-register'))->name('register');
