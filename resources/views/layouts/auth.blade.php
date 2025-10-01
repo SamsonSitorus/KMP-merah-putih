612c459da68c4ec63390b44802cf8151bd194c3c
SamsonSitorus <149450441+SamsonSitorus@users.noreply.github.com>
2025-10-01 20:10:56 +0700

    de
    de

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en" data-assets-path="{{ asset('assets/') }}">
+  @include('layouts.head')
+
+  <body>
+    <div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          {{-- Konten login/register --}}
+          @yield('content')
+        </div>
+      </div>
+    </div>
+
+    @include('layouts.scripts')
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::get('/login', fn() => view('auth.login'))->name('login');
 Route::post('/login', [Authcontroller::class,'login']);
 
 Route::get('/register', fn() => view('auth.auth-register'))->name('register');
+Route::get('/home', fn() => view('user.home'))->name('home');
