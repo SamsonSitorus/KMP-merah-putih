82ff8b47d466cd235a814fc6d62b68602df1dbf1
dicky245 <dickyhutajulu905@gmail.com>
2025-12-24 10:42:25 +0700

    dada

--- database/migrations/2025_12_23_062905_create_booking_passenger_table.php
+++ database/migrations/2025_12_23_062905_create_booking_passenger_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('booking_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('category');
+            // $table->string('category');
             $table->timestamps();
         });
     }
